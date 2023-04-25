c683ad623f688a2e3a4005d96c94487e7d5b8aba
Shubham-andy7 <67570264+Shubham-andy7@users.noreply.github.com>
2023-04-26 02:43:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,11 @@ def roll(sides):
 
 posts = []
 
-@app.route('/post', methods=['POST'])
+@app.post('/post')
 def create_post():
-    # Check if request is a JSON object with msg field
-    if not request.json or 'msg' not in request.json:
+    message = request.get_json(force = True)
+    if not request.json or 'msg' not in message:
         abort(400)
-
-    # Create new post
     post_id = len(posts) + 1
     key = secrets.token_hex(16)
     timestamp = datetime.datetime.utcnow().replace(microsecond=0).isoformat() + 'Z'
@@ -28,52 +26,42 @@ def create_post():
         'id': post_id,
         'key': key,
         'timestamp': timestamp,
-        'msg': request.json['msg']
+        'msg': message['msg']
     }
     posts.append(post)
 
     return jsonify(post), 201
 
-# Endpoint 2: Read a post with given ID
 @app.route('/post/<int:post_id>', methods=['GET'])
 def read_post(post_id):
-    # Find post with given ID
     post = next((p for p in posts if p['id'] == post_id), None)
-
-    # If post doesn't exist, return error 404
     if not post:
         abort(404)
-
-    # Return post without key
     return jsonify({'id': post['id'], 'timestamp': post['timestamp'], 'msg': post['msg']})
 
-# Endpoint 3: Delete a post with given ID and key
 @app.route('/post/<int:post_id>/delete/<string:key>', methods=['DELETE'])
 def delete_post(post_id, key):
-    # Find post with given ID
     post = next((p for p in posts if p['id'] == post_id), None)
-
-    # If post doesn't exist, return error 404
     if not post:
         abort(404)
-
-    # If key doesn't match, return error 403
     if post['key'] != key:
         abort(403)
-
-    # Delete post
     posts.remove(post)
+    print(posts)
 
-    # Return same information as creating a post
-    new_post = {
-        'id': post_id,
-        'key': secrets.token_hex(16),
-        'timestamp': datetime.datetime.utcnow().replace(microsecond=0).isoformat() + 'Z',
-        'msg': post['msg']
-    }
-    posts.append(new_post)
-
-    return jsonify(new_post)
+    return jsonify(post)
 
 if __name__ == '__main__':
     app.run()   
+
+
+#Commands
+'''
+curl http://localhost:5000/random/6
+
+Invoke-WebRequest -Uri http://localhost:5000/post -Method POST -Body '{"msg": "Hello, world!"}' -Headers @{"Content-Type"="application/json"}
+
+curl http://localhost:5000/post/<int>
+
+Invoke-WebRequest -Uri http://localhost:5000/post/<int>/delete/<string> -Method DELETE
+'''
