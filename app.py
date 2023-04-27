b5ae8f5a44134c6c560af7a8355eac27e11d5922
Shubham-andy7 <67570264+Shubham-andy7@users.noreply.github.com>
2023-04-28 01:15:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import datetime
 from secrets import randbelow
 
 app = Flask(__name__)
-users = {}
-
 
 @app.get("/random/<int:sides>")
 def roll(sides):
@@ -15,61 +13,72 @@ def roll(sides):
     return { 'num': randbelow(sides) + 1 }
 
 posts = []
+users = {}
 
 @app.post('/post')
 def create_post():
     message = request.get_json(force = True)
     if not request.json or 'msg' not in message:
         abort(400)
-    if 'user_id' in message and 'user_key' in message:
-        user_id = message['user_id']
-        user_key = message['user_key']
-        if user_id not in users or users[user_id] != user_key:
-            abort(403)
     post_id = len(posts) + 1
     key = secrets.token_hex(16)
     timestamp = datetime.datetime.utcnow().replace(microsecond=0).isoformat() + 'Z'
+
+    #Extension - 1(User and User key)
     if 'user_id' in message and 'user_key' in message:
+        user_id = message['user_id']
+        user_key = message['user_key']
+        if user_id not in users:
+            users['user_id'] = user_id
+            users['user_key'] = [user_key]
+        else:
+            users['user_key'].append(user_key)
+
         post = {
             'id': post_id,
+            'key': key,
+            'timestamp': timestamp,
+            'msg': message['msg'],
             'user_id': user_id,
-            'key': key,
-            'timestamp': timestamp,
-            'msg': message['msg']
+            'user_key': user_key
         }
-    else:
-        post = {
-            'id': post_id,
-            'key': key,
-            'timestamp': timestamp,
-            'msg': message['msg']
-        }
+        posts.append(post)
+        return jsonify(post), 201
+    
+    if ('user_id' in message and 'user_key' not in message) or ('user_id' not in message and 'user_key' in message):
+        return {'err': 'Please enter both User id and User Key'}, 400
+    
+    post = {
+        'id': post_id,
+        'key': key,
+        'timestamp': timestamp,
+        'msg': message['msg']
+    }
     posts.append(post)
 
     return jsonify(post), 201
 
-
-@app.post('/user')
-def create_user():
-    user_id = len(users) + 1
-    key = secrets.token_hex(16)
-    users[user_id] = key
-    return jsonify({'user_id': user_id, 'key': key}), 201
-
-
 @app.route('/post/<int:post_id>', methods=['GET'])
-def read_post(post_id):
-    post = next((p for p in posts if p['id'] == post_id), None)
+@app.route('/post/<int:post_id>/<string:user_id>/<string:user_key>', methods=['GET'])
+def read_post(post_id, user_id=None, user_key=None):
+    if user_id is None and user_key is None:
+        post = next((p for p in posts if p['id'] == post_id), None)
+    else:
+        post = next((p for p in posts if (p['id'] == post_id and p['user_id'] == user_id and p['user_key'] == user_key)), None)
     if not post:
         abort(404)
-    if 'user_id' in post:
-        user_id = post['user_id']
-        return jsonify({'id': post['id'], 'user_id': user_id, 'timestamp': post['timestamp'], 'msg': post['msg']})
-    return jsonify({'id': post['id'], 'timestamp': post['timestamp'], 'msg': post['msg']})
+    if user_id is None and user_key is None:
+        return jsonify({'id': post['id'], 'timestamp': post['timestamp'], 'msg': post['msg']})
+    else:
+        return jsonify({'id': post['id'], 'timestamp': post['timestamp'], 'msg': post['msg'], 'user_id': post['user_id'], 'user_key': post['user_key']})
 
 @app.route('/post/<int:post_id>/delete/<string:key>', methods=['DELETE'])
-def delete_post(post_id, key):
-    post = next((p for p in posts if p['id'] == post_id), None)
+@app.route('/post/<int:post_id>/delete/<string:key>/<string:user_id>/<string:user_key>', methods=['DELETE'])
+def delete_post(post_id, key, user_id=None, user_key=None):
+    if user_id is None and user_key is None:
+        post = next((p for p in posts if p['id'] == post_id), None)
+    else:
+        post = next((p for p in posts if (p['id'] == post_id and p['user_id'] == user_id and p['user_key'] == user_key)), None)
     if not post:
         abort(404)
     if post['key'] != key:
@@ -79,39 +88,5 @@ def delete_post(post_id, key):
 
     return jsonify(post)
 
-'''
-@app.route('/post/<int:post_id>/delete', methods=['DELETE'])
-def delete_post(post_id):
-    post = next((p for p in posts if p['id'] == post_id), None)
-    if not post:
-        abort(404)
-    key = request.args.get('key')
-    user_id = request.args.get('user_id')
-    user_key = request.args.get('user_key')
-    if not key and not (user_id and user_key):
-        abort(400)
-    if key:
-        if post['key'] != key:
-            abort(403)
-    else:
-        if user_id not in users or users[user_id] != user_key:
-            abort(403)
-    posts.remove(post)
-
-    return jsonify(post)
-'''
-
 if __name__ == '__main__':
     app.run()   
-
-
-#Commands
-'''
-curl http://localhost:5000/random/6
-
-Invoke-WebRequest -Uri http://localhost:5000/post -Method POST -Body '{"msg": "Hello, world!"}' -Headers @{"Content-Type"="application/json"}
-
-curl http://localhost:5000/post/<int>
-
-Invoke-WebRequest -Uri http://localhost:5000/post/<int>/delete/<string> -Method DELETE
-'''
