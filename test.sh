5140693df6590ca707be1f2c9a401c99c6446a2b
Shubham-andy7 <67570264+Shubham-andy7@users.noreply.github.com>
2023-05-02 04:37:18 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,37 +1,40 @@
 #!/bin/sh
 
-set -e # exit immediately if newman complains
-trap 'kill $PID' EXIT # kill the server on exit
-
-./run.sh &
-PID=$! # record the PID
-## install jq before running this shell script : for mac it is brew install jq
-#Baseline - 1 create post
-response=$(curl -s -X POST -H "Content-Type: application/json" -d '{"msg": "demon slayer__0, world!"}' http://127.0.0.1:5000/post)
+#exec flask run
+echo "Baseline - 1 create post"
+response=$(curl -s -X POST -H "Content-Type: application/json" -d '{"msg": "demon slayer__0, world!"}' http://localhost:5000/post)
 key=$(echo $response | jq -r '.key')
 id=$(echo $response | jq -r '.id')
 echo $response
 echo $key
 
-##Baseline - 2 read post
-get_response=$(curl http://127.0.0.1:5000/post/$id)
+echo "Baseline - 2 read post"
+get_response=$(curl http://localhost:5000/post/$id)
 echo $get_response
+#
+echo "Extension - 1 User and User Keys"
+user_id_key_reps=$(curl -s -X POST -H "Content-Type: application/json" -d '{"msg": "demon slayer__0, world!", "user_id": "123", "user_key": "12345"}' http://localhost:5000/post)
+echo $user_id_key_reps
+start_date_str=$(echo $response | jq -r '.timestamp')
+echo $start_date_str
 
-#Extension - 1 User and User Keys
-post =  curl -X POST -H "Content-Type: application/json" -d '{"msg": "demon slayer__0, world!", "user_id": "123", "user_key": "12345"}' http://127.0.0.1:5000/post
 
-#Extension - 2 Threaded Replies
-post =  curl -X POST -H "Content-Type: application/json" -d '{"msg": "demon slayer__0, world!", "reply_to_id": 1}' http://127.0.0.1:5000/post
-curl http://127.0.0.1:5000/post/1
+echo "Extension - 2 Threaded Replies"
+treplies=$(curl -s -X POST -H "Content-Type: application/json" -d '{"msg": "demon slayer__0, world!", "reply_to_id": 1}' http://localhost:5000/post)
+echo $treplies
+reply_to_id=$(echo $treplies | jq -r '.reply_to_id')
+echo $(curl http://localhost:5000/post/$reply_to_id)
 
-#Extension - 3 DateTime Based Queries
-curl -X GET "http://127.0.0.1:5000/posts?start_date_time=2023-05-01T01:30:47Z"
-#Extension - 4 User Based Queries
-curl http://127.0.0.1:5000/post/"123"/"12345"
+echo "Extension - 3 DateTime Based Queries"
+curl -X GET "http://localhost:5000/posts?start_date_time=$start_date_str"
 
-#Extension - 5 FullText Search
-curl http://127.0.0.1:5000/post/"demon slayer__0, world!"
 
-##Baseline - 3 delete post
-echo "$key"
-curl -X DELETE http://127.0.0.1:5000/post/1/delete/"$key"
+###Extension - 4 User Based Queries
+##
+
+echo "Extension - 5 FullText Search"
+echo $(curl http://localhost:5000/post/"demon slayer__0, world!")
+
+#echo "Baseline - 3 delete post"
+#echo "$key"
+#curl -X DELETE http://localhost:5000/post/1/delete/"$key"
