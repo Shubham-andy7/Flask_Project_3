f683e3e2cb91ad318ec352b4c68cc531ca3b12e8
Shubham-andy7 <67570264+Shubham-andy7@users.noreply.github.com>
2023-05-01 22:55:01 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-set -e # exit immediately if newman complains
-trap 'kill $PID' EXIT # kill the server on exit
+#Baseline - 1 create post
 
-./run.sh &
-PID=$! # record the PID
+#Baseline - 2 read post
 
-newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
-newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
+#Baseline - 3 delete post
+
+#Extension - 1 User and User Keys
+
+#Extension - 2 Threaded Replies
+
+#Extension - 3 DateTime Based Queries
+
+#Extension - 4 User Based Queries
+
+#Extension - 5 FullText Search
