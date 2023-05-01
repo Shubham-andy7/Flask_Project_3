389fe3246f672b0ae63e3c5bf77da55bcf364736
mrinChanSIT <114021625+mrinChanSIT@users.noreply.github.com>
2023-05-01 16:09:46 -0400

    Extension-3 posts between date ranges

--- run.sh
+++ run.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
 #Baseline - 1 create post
-response=$(curl -X POST -H "Content-Type: application/json" -d '{"msg": "demon slayer__0, world!"}' http://localhost:5000/post)
-
-#Baseline - 2 read post
-curl http://localhost:5000/post/1
-
-#Baseline - 3 delete post
-curl http://localhost:5000//post/1/delete/<string:key>
-#Extension - 1 User and User Keys
-
-#Extension - 2 Threaded Replies
-
-#Extension - 3 DateTime Based Queries
-curl -X GET "http://localhost:5000/posts?start_date_time=2023-05-01T01:30:47Z"
-#Extension - 4 User Based Queries
-
-#Extension - 5 FullText Search
+response=$(curl -s -X POST -H "Content-Type: application/json" -d '{"msg": "demon slayer__0, world!"}' http://localhost:5000/post)
+key=$(echo $response | jq -r '.key')
+id=$(echo $response | jq -r '.id')
+echo $response
+echo $key
+##Baseline - 2 read post
+get_response=$(curl http://localhost:5000/post/$id)
+echo $get_response
+#
+##Baseline - 3 delete post
+echo "$key"
+curl -X DELETE http://localhost:5000/post/1/delete/"$key"
+##Extension - 1 User and User Keys
+#
+##Extension - 2 Threaded Replies
+#
+##Extension - 3 DateTime Based Queries
+#curl -X GET "http://localhost:5000/posts?start_date_time=2023-05-01T01:30:47Z"
+##Extension - 4 User Based Queries
+#
+##Extension - 5 FullText Search
