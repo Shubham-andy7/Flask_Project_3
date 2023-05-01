eb2b2a0af5a8cd243532eecd3b2e4a5c3419f93a
mrinChanSIT <114021625+mrinChanSIT@users.noreply.github.com>
2023-05-01 16:44:10 -0400

    Extension-3 posts between date ranges

--- run.sh
+++ run.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 ## install jq before running this shell script : for mac it is brew install jq
-#Baseline - 1 create post
+echo "Baseline - 1 create post"
 response=$(curl -s -X POST -H "Content-Type: application/json" -d '{"msg": "demon slayer__0, world!"}' http://localhost:5000/post)
 key=$(echo $response | jq -r '.key')
 id=$(echo $response | jq -r '.id')
 echo $response
 echo $key
-##Baseline - 2 read post
+
+echo "Baseline - 2 read post"
 get_response=$(curl http://localhost:5000/post/$id)
 echo $get_response
 #
-##Baseline - 3 delete post
+echo "Baseline - 3 delete post"
 echo "$key"
 curl -X DELETE http://localhost:5000/post/1/delete/"$key"
-##Extension - 1 User and User Keys
-#
+
+echo "Extension - 1 User and User Keys"
+user_id_key_reps=$(curl -s -X POST -H "Content-Type: application/json" -d '{"msg": "demon slayer__0, world!", "user_id": "123", "user_key": "12345"}' http://localhost:5000/post)
+start_date_time=$(echo user_id_key_reps | jq -r '.timestamp')
+echo $start_date_time
 ##Extension - 2 Threaded Replies
 #
-##Extension - 3 DateTime Based Queries
-#curl -X GET "http://localhost:5000/posts?start_date_time=2023-05-01T01:30:47Z"
+echo "Extension - 3 DateTime Based Queries"
+curl -X GET "http://localhost:5000/posts?start_date_time=$start_date_time"
 ##Extension - 4 User Based Queries
 #
 ##Extension - 5 FullText Search
