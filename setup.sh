3b1670d7fd4c767acfca93f951fa2e0c3baf6ce1
Shubham-andy7 <67570264+Shubham-andy7@users.noreply.github.com>
2023-05-02 04:55:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pip3 install flask
-# pip3 install secrets
-pip3 install datetime
-pip3 install jq
+# pip3 install flask
+# # pip3 install secrets
+# pip3 install datetime
+# pip3 install jq
