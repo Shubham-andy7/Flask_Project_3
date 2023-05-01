d1f37e057ae8e7a616d68db37a1340d6ff3975f2
mrinChanSIT <114021625+mrinChanSIT@users.noreply.github.com>
2023-05-01 15:32:06 -0400

    Extension-3 posts between date ranges

--- run.sh
+++ run.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 #Baseline - 1 create post
+post = curl -X POST -H "Content-Type: application/json" -d '{"msg": "demon slayer__0, world!"}' http://localhost:5000/post
 
 #Baseline - 2 read post
+curl http://localhost:5000/post/1
 
 #Baseline - 3 delete post
-
+curl http://localhost:5000//post/1/delete/<string:key>
 #Extension - 1 User and User Keys
 
 #Extension - 2 Threaded Replies
 
 #Extension - 3 DateTime Based Queries
-
+curl -X GET "http://localhost:5000/posts?start_date_time=2023-05-01T01:30:47Z"
 #Extension - 4 User Based Queries
 
 #Extension - 5 FullText Search
