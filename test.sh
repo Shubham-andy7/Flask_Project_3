6fd8cd1fa704764a9acaadbdeeb127965391c52e
Shubham-andy7 <67570264+Shubham-andy7@users.noreply.github.com>
2023-05-02 07:52:10 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-exec flask run
+set -e # exit immediately if newman complains
+trap 'kill $PID' EXIT # kill the server on exit
 
+./run.sh &
+PID=$!
+#exec flask run
 echo "Baseline - 1 create post"
 response=$(curl -s -X POST -H "Content-Type: application/json" -d '{"msg": "demon slayer__0, world!"}' http://localhost:5000/post)
 key=$(echo $response | jq -r '.key')
@@ -14,7 +18,7 @@ get_response=$(curl http://localhost:5000/post/$id)
 echo $get_response
 #
 echo "Extension - 1 User and User Keys"
-user_id_key_reps=$(curl -s -X POST -H "Content-Type: application/json" -d '{"msg": "demon slayer__0, world!", "user_id": "123", "user_key": "12345"}' http://localhost:5000/post)
+user_id_key_reps=$(curl -s -X POST -H "Content-Type: application/json" -d '{"msg": "demon slayer__0, world!", "user_id": "123", "user_key": 12345}' http://localhost:5000/post)
 echo $user_id_key_reps
 start_date_str=$(echo $response | jq -r '.timestamp')
 echo $start_date_str
@@ -29,9 +33,8 @@ echo $(curl http://localhost:5000/post/$reply_to_id)
 echo "Extension - 3 DateTime Based Queries"
 curl -X GET "http://localhost:5000/posts?start_date_time=$start_date_str"
 
-
-###Extension - 4 User Based Queries
-##
+echo Extension - 4 User Based Queries
+echo $(curl http://localhost:5000/post/'123'/12345)
 
 echo "Extension - 5 FullText Search"
 echo $(curl http://localhost:5000/post/"demon slayer__0, world!")
@@ -39,5 +42,3 @@ echo $(curl http://localhost:5000/post/"demon slayer__0, world!")
 echo "Baseline - 3 delete post"
 echo "$key"
 curl -X DELETE http://localhost:5000/post/1/delete/"$key"
-
-killall flask
